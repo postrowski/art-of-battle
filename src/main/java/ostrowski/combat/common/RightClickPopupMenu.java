e39886056351fae92d61024536389b4d4ad3208f
postrowski <paul@ostrowski.com>
2019-12-28 20:08:19 -0700

    improved right-click map options

--- src/main/java/ostrowski/combat/common/RightClickPopupMenu.java
+++ src/main/java/ostrowski/combat/common/RightClickPopupMenu.java
@@ -12,6 +12,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
@@ -33,7 +34,9 @@ import org.xml.sax.SAXException;
 
 import ostrowski.combat.common.enums.AI_Type;
 import ostrowski.combat.common.enums.Enums;
+import ostrowski.combat.common.enums.Facing;
 import ostrowski.combat.common.enums.TerrainType;
+import ostrowski.combat.common.orientations.Orientation;
 import ostrowski.combat.common.things.Thing;
 import ostrowski.combat.server.Arena;
 import ostrowski.combat.server.ArenaLocation;
@@ -48,24 +51,18 @@ public class RightClickPopupMenu
 
    private ArenaLocation _currentMouseLoc = null;
    private final Arena _arena;
-   private Menu _characterPopupMenu;
+   private Menu _popupMenu;
+   private Menu _moveToPopupMenu;
+   private MenuItem _characterPopupMenu;
 
-   private MenuItem _changeControlMenuItem;
    private final Map<AI_Type, MenuItem> _controlMenuSubItem = new HashMap<>();
    private MenuItem _aiLocal;
    private MenuItem _aiRemote;
-   private MenuItem _changeTeamMenuItem;
    private final Map<String, MenuItem> _teamMenuSubItem = new HashMap<>();
-   private MenuItem _relocateCharacter;
-   private MenuItem _removeCharacter;
-   private MenuItem _loadCharacter;
-   private MenuItem _saveCharacter;
-   private MenuItem _editCharacterCondition;
-   private MenuItem _addCharacter;
-   private MenuItem _addItem;
    private MenuItem _removeItems;
-   private MenuItem _terrainMenuItem;
+   private MenuItem _addCharacter;
    private final Map<TerrainType, MenuItem> _setTerrain = new HashMap<>();
+   private ArenaLocation _moveFromLocation = null;
 
    public RightClickPopupMenu(Arena arena) {
       this._arena = arena;
@@ -80,69 +77,35 @@ public class RightClickPopupMenu
          if (shell == null) {
             return;
          }
-         if ((_characterPopupMenu == null) || _characterPopupMenu.isDisposed()) {
-            _characterPopupMenu = new Menu(shell);
-            {
-               _changeControlMenuItem = createMenu(_characterPopupMenu, "Change Control", null, SWT.CASCADE);
-               Menu subMenuChangeControl = new Menu(_changeControlMenuItem);
-               _changeControlMenuItem.setMenu(subMenuChangeControl);
-               _aiRemote = createMenu(subMenuChangeControl, "Remote Connection", e -> changeToRemoteConnection(), SWT.CHECK);
-               _aiLocal = createMenu(subMenuChangeControl, "Local Control", e -> changeControl(null), SWT.CHECK);
-               for (AI_Type ai : AI_Type.values()) {
-                  _controlMenuSubItem.put(ai, createMenu(subMenuChangeControl, ai.name(),
-                                                         e -> changeControl(ai), SWT.CHECK));
-               }
+         if (_moveFromLocation == null) {
+            if ((_popupMenu == null) || _popupMenu.isDisposed()) {
+               createMainMenu(shell);
             }
-            {
-               _changeTeamMenuItem = createMenu(_characterPopupMenu, "Change Team", null, SWT.CASCADE);
-               Menu subMenuChangeTeam = new Menu(_changeTeamMenuItem);
-               _changeTeamMenuItem.setMenu(subMenuChangeTeam);
-               for (String teamName : Enums.TEAM_NAMES) {
-                  _teamMenuSubItem.put(teamName, createMenu(subMenuChangeTeam, teamName,
-                                                            e -> changeTeam(teamName), SWT.CHECK));
-               }
-            }
-
-            _relocateCharacter = createMenu(_characterPopupMenu, "Relocate Character", e -> relocateCharacter(), SWT.NONE);
-            _removeCharacter = createMenu(_characterPopupMenu, "Remove Character", e -> removeCharacter(), SWT.NONE);
-            _loadCharacter = createMenu(_characterPopupMenu, "Load Character in editor",e -> editCharacter(true), SWT.NONE);
-            _saveCharacter = createMenu(_characterPopupMenu, "Save Character from editor", e -> editCharacter(false), SWT.NONE);
-            _editCharacterCondition = createMenu(_characterPopupMenu, "Edit Character Condition", e -> editCharacterCondition(), SWT.NONE);
-
-            _addCharacter = createMenu(_characterPopupMenu, "Add Character", e -> addCharacter(), SWT.NONE);
-            _addItem = createMenu(_characterPopupMenu, "Add Item", e -> addItem(), SWT.NONE);
-            {
-               _terrainMenuItem = createMenu(_characterPopupMenu, "Terrain", null, SWT.CASCADE);
-               Menu subMenuEditTerrain = new Menu(_terrainMenuItem);
-               _terrainMenuItem.setMenu(subMenuEditTerrain);
-               for (TerrainType terrainType : TerrainType.values()) {
-                  _setTerrain.put(terrainType, createMenu(subMenuEditTerrain, terrainType.name(),
-                                                          e -> setTerrain(terrainType), SWT.CHECK));
-               }
-            }
-            _removeItems = createMenu(_characterPopupMenu, "Remove all items", e -> addRemoveItems(), SWT.NONE);
          }
-         _characterPopupMenu.setVisible(false);
+         else {
+            if ((_moveToPopupMenu == null) || _moveToPopupMenu.isDisposed()) {
+               createMoveToMenu(shell);
+            }
+         }
+
+         _popupMenu.setVisible(false);
+         _moveToPopupMenu.setVisible(false);
       }
       if (loc != null) {
          List<Character> characters = loc.getCharacters();
          List<Object> things = loc.getThings();
          boolean characterPresent = !characters.isEmpty();
-         _changeControlMenuItem.setEnabled(characterPresent);
-         _changeTeamMenuItem.setEnabled(characterPresent);
+         this._characterPopupMenu.setEnabled(characterPresent);
          _addCharacter.setEnabled(!characterPresent);
-         _relocateCharacter.setEnabled(characterPresent);
-         _loadCharacter.setEnabled(characterPresent);
-         _saveCharacter.setEnabled(characterPresent);
-         _removeCharacter.setEnabled(characterPresent);
-         _editCharacterCondition.setEnabled(characterPresent);
          if (characterPresent) {
             Character character = loc.getCharacters().get(0);
+            // Select the character's current team
             for (String teamName : Enums.TEAM_NAMES) {
                boolean selected = Enums.TEAM_NAMES[character._teamID].equals(teamName);
                _teamMenuSubItem.get(teamName).setSelection(selected);
             }
 
+            // Select the character's current AI/remote/local Control
             _aiRemote.setSelection(false);
             _aiLocal.setSelection(false);
             for (AI_Type aiType : AI_Type.values()) {
@@ -161,16 +124,90 @@ public class RightClickPopupMenu
                }
             }
          }
-         _characterPopupMenu.setLocation(Display.getCurrent().getCursorLocation().x,
-                                         Display.getCurrent().getCursorLocation().y);
-         _characterPopupMenu.setVisible(true);
-         _currentMouseLoc = loc;
-
-         _removeItems.setEnabled(things.size() > characters.size());
-
+         // Select the current location's Terrain
          for (TerrainType terrainType : TerrainType.values()) {
             boolean selected = _currentMouseLoc.getTerrain() == terrainType;
             _setTerrain.get(terrainType).setSelection(selected);
+         }
+         // enable/disable the 'remove all items' menu item.
+         _removeItems.setEnabled(things.size() > characters.size());
+         _currentMouseLoc = loc;
+      }
+      Point cursorLocation = Display.getCurrent().getCursorLocation();
+      if (_moveFromLocation == null) {
+         _popupMenu.setLocation(cursorLocation.x, cursorLocation.y);
+      }
+      else {
+         _moveToPopupMenu.setLocation(cursorLocation.x, cursorLocation.y);
+      }
+
+      _moveToPopupMenu.setVisible(_moveFromLocation != null);
+      _popupMenu.setVisible(_moveFromLocation == null);
+   }
+
+   private void createMainMenu(Shell shell) {
+      _popupMenu = new Menu(shell);
+      {
+         _characterPopupMenu = createMenu(_popupMenu, "Character", null, SWT.CASCADE);
+         Menu subMenuCharacter = new Menu(_characterPopupMenu);
+         _characterPopupMenu.setMenu(subMenuCharacter);
+         {
+            MenuItem changeControlMenuItem = createMenu(subMenuCharacter, "Change Control", null, SWT.CASCADE);
+            Menu subMenuChangeControl = new Menu(changeControlMenuItem);
+            changeControlMenuItem.setMenu(subMenuChangeControl);
+            _aiRemote = createMenu(subMenuChangeControl, "Remote Connection", e -> changeToRemoteConnection(), SWT.CHECK);
+            _aiLocal = createMenu(subMenuChangeControl, "Local Control", e -> changeControl(null), SWT.CHECK);
+            for (AI_Type ai : AI_Type.values()) {
+               _controlMenuSubItem.put(ai, createMenu(subMenuChangeControl, ai.name(),
+                                                      e -> changeControl(ai), SWT.CHECK));
+            }
+         }
+         {
+            MenuItem changeTeamMenuItem = createMenu(subMenuCharacter, "Change Team", null, SWT.CASCADE);
+            Menu subMenuChangeTeam = new Menu(changeTeamMenuItem);
+            changeTeamMenuItem.setMenu(subMenuChangeTeam);
+            for (String teamName : Enums.TEAM_NAMES) {
+               _teamMenuSubItem.put(teamName, createMenu(subMenuChangeTeam, teamName,
+                                                         e -> changeTeam(teamName), SWT.CHECK));
+            }
+         }
+
+         createMenu(subMenuCharacter, "Relocate", e -> _moveFromLocation = _currentMouseLoc, SWT.NONE);
+         createMenu(subMenuCharacter, "Load into editor", e -> editCharacter(true), SWT.NONE);
+         createMenu(subMenuCharacter, "Save from editor", e -> editCharacter(false), SWT.NONE);
+         createMenu(subMenuCharacter, "Edit Condition", e -> editCharacterCondition(), SWT.NONE);
+         createMenu(subMenuCharacter, "Remove", e -> removeCharacter(), SWT.NONE);
+      }
+      createMenu(_popupMenu, "Add item", e -> addItem(), SWT.NONE);
+      _addCharacter = createMenu(_popupMenu, "Add character", e -> addCharacter(), SWT.NONE);
+      _removeItems = createMenu(_popupMenu, "Remove all items", e -> addRemoveItems(), SWT.NONE);
+      {
+         MenuItem _terrainMenuItem = createMenu(_popupMenu, "Terrain", null, SWT.CASCADE);
+         Menu subMenuEditTerrain = new Menu(_terrainMenuItem);
+         _terrainMenuItem.setMenu(subMenuEditTerrain);
+         for (TerrainType terrainType : TerrainType.values()) {
+            _setTerrain.put(terrainType, createMenu(subMenuEditTerrain, terrainType.name(),
+                                                    e -> setTerrain(terrainType), SWT.CHECK));
+         }
+      }
+   }
+   private void createMoveToMenu(Shell shell) {
+      _moveToPopupMenu = new Menu(shell);
+      createMenu(_popupMenu, "Relocate to here, facing 12 O'Clock", e -> completeMove(Facing.NOON), SWT.NONE);
+      createMenu(_popupMenu, "Relocate to here, facing  2 O'Clock", e -> completeMove(Facing._2_OCLOCK), SWT.NONE);
+      createMenu(_popupMenu, "Relocate to here, facing  4 O'Clock", e -> completeMove(Facing._4_OCLOCK), SWT.NONE);
+      createMenu(_popupMenu, "Relocate to here, facing  6 O'Clock", e -> completeMove(Facing._6_OCLOCK), SWT.NONE);
+      createMenu(_popupMenu, "Relocate to here, facing  8 O'Clock", e -> completeMove(Facing._8_OCLOCK), SWT.NONE);
+      createMenu(_popupMenu, "Relocate to here, facing 10 O'Clock", e -> completeMove(Facing._10_OCLOCK), SWT.NONE);
+      createMenu(_popupMenu, "Cancel Relocation", e -> _moveFromLocation = null, SWT.NONE);
+   }
+
+   private void completeMove(Facing facing) {
+      Character character = getCharacter();
+      if (character != null) {
+         Orientation orient = character.getOrientation();
+         if (orient.setHeadLocation(character, _currentMouseLoc, facing, _arena.getCombatMap(), null, true)) {
+            _moveFromLocation = null;
          }
       }
    }
@@ -189,9 +226,8 @@ public class RightClickPopupMenu
    }
 
    private void addRemoveItems() {
-      ArrayList<Object> things = new ArrayList<>();
-      things.addAll(_currentMouseLoc.getThings());
-      for (Object thing : things) {
+      // be sure we don't remove any characters
+      for (Object thing : new ArrayList<>(_currentMouseLoc.getThings())) {
          if (!(thing instanceof Character)) {
             _currentMouseLoc.remove(thing);
          }
@@ -200,15 +236,12 @@ public class RightClickPopupMenu
 
    private void addItem() {
       openDialog(Display.getCurrent(), "Add Item", "Item to add:", "", false /*multiLine*/,
-                 new IActOnTextResults() {
-         @Override
-         public void doAction(String results) {
-            Thing thing = Thing.getThing(results, Race.getRace(Race.NAME_Human, Race.Gender.MALE));
-            if (thing != null) {
-               _currentMouseLoc.addThing(thing);
-            }
-         }
-      });
+                 results -> {
+                    Thing thing = Thing.getThing(results, Race.getRace(Race.NAME_Human, Race.Gender.MALE));
+                    if (thing != null) {
+                       _currentMouseLoc.addThing(thing);
+                    }
+                 });
    }
 
    private void changeTeam(String newTeamName) {
@@ -225,6 +258,7 @@ public class RightClickPopupMenu
             }
             team++;
          }
+         this._arena.recomputeAllTargets(character);
       }
    }
 
@@ -263,10 +297,9 @@ public class RightClickPopupMenu
          factory.setValidating(true/*validating*/);
 
          // Create the builder and parse the file
-         Document charConditionDoc = null;
          try {
             DocumentBuilder builder = factory.newDocumentBuilder();
-            charConditionDoc = builder.newDocument();
+            Document charConditionDoc = builder.newDocument();
             Element mainElement = originalCharacter.getCondition().getXMLObject(charConditionDoc, "\n");
 
             DOMImplementationLS domImplLS = (DOMImplementationLS) charConditionDoc.getImplementation();
@@ -275,21 +308,18 @@ public class RightClickPopupMenu
 
             // Open a dialog to edit the condition
             openDialog(Display.getCurrent(), "Edit Condition", "Player condition:", xmlStr, true /*multiLine*/,
-                       new IActOnTextResults() {
-               @Override
-               public void doAction(String results) {
-                  InputSource src = new InputSource(new StringReader(results));
-                  try {
-                     Document doc = factory.newDocumentBuilder().parse(src);
-                     character.getCondition().serializeFromXmlObject(doc.getFirstChild());
+                       results -> {
+                          InputSource src = new InputSource(new StringReader(results));
+                          try {
+                             Document doc = factory.newDocumentBuilder().parse(src);
+                             character.getCondition().serializeFromXmlObject(doc.getFirstChild());
 
-                     ObjectChanged changeNotif = new ObjectChanged(originalCharacter, character);
-                     character.notifyWatchers(originalCharacter, character, changeNotif, null/*skipList*/, null/*diag*/);
-                  } catch (SAXException | IOException | ParserConfigurationException e) {
-                     e.printStackTrace();
-                  }
-               }
-            });
+                             ObjectChanged changeNotif = new ObjectChanged(originalCharacter, character);
+                             character.notifyWatchers(originalCharacter, character, changeNotif, null/*skipList*/, null/*diag*/);
+                          } catch (SAXException | IOException | ParserConfigurationException e) {
+                             e.printStackTrace();
+                          }
+                       });
 
          } catch (ParserConfigurationException e) {
             e.printStackTrace();
@@ -305,16 +335,11 @@ public class RightClickPopupMenu
       }
    }
 
-   private void relocateCharacter() {
-      Character character = getCharacter();
-      if (character != null) {
-      }
-   }
-
    private void changeControl(AI_Type ai) {
       Character character = getCharacter();
       if (character != null) {
          this._arena.setControl(character, true/*localControl*/, ai);
+         this._arena.recomputeAllTargets(character);
       }
    }
    private void changeToRemoteConnection() {
@@ -325,8 +350,9 @@ public class RightClickPopupMenu
    }
 
    private Character getCharacter() {
-      if (_currentMouseLoc != null) {
-         List<Character> chars = _currentMouseLoc.getCharacters();
+      ArenaLocation loc = (_moveFromLocation == null) ? _currentMouseLoc : _moveFromLocation;
+      if (loc != null) {
+         List<Character> chars = loc.getCharacters();
          if (!chars.isEmpty()) {
             return chars.get(0);
          }
@@ -334,7 +360,7 @@ public class RightClickPopupMenu
       return null;
    }
 
-   static private String openDialog(Display shell, String title, String prompt, String contents,
+   static private void openDialog(Display shell, String title, String prompt, String contents,
                                     boolean multiline, IActOnTextResults actOnResult) {
       final Shell dialog = new Shell (shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
       dialog.setText(title);
@@ -356,12 +382,7 @@ public class RightClickPopupMenu
       data.right = new FormAttachment (100, 0);
       data.bottom = new FormAttachment (100, 0);
       cancel.setLayoutData (data);
-      cancel.addListener(SWT.Selection, new Listener() {
-         @Override
-         public void handleEvent(Event e) {
-            dialog.close();
-         }
-      });
+      cancel.addListener(SWT.Selection, e -> dialog.close());
 
       int style = SWT.BORDER;
       if (multiline) {
@@ -384,17 +405,13 @@ public class RightClickPopupMenu
       data.right = new FormAttachment (cancel, 0, SWT.DEFAULT);
       data.bottom = new FormAttachment (100, 0);
       ok.setLayoutData (data);
-      ok.addListener(SWT.Selection, new Listener() {
-         @Override
-         public void handleEvent(Event e) {
-            actOnResult.doAction(text.getText());
-            dialog.close();
-         }
+      ok.addListener(SWT.Selection, e -> {
+         actOnResult.doAction(text.getText());
+         dialog.close();
       });
 
       dialog.setDefaultButton (ok);
       dialog.pack ();
       dialog.open ();
-      return text.getText();
    }
 }
