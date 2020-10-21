e78130d468ada9d68b322e6f24293820f5ab5053
postrowski <paul@ostrowski.com>
2020-10-20 20:40:28 -0700

    added test classes

---
+++ src/test/java/ostrowski/combat/common/AiTest.java
@@ -0,0 +1,15 @@
+package ostrowski.combat.common;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AiTest
+{
+
+   @Test
+   public void testFindPotionIndexToUse() {
+      assertTrue("First implemented", true);
+   }
+
+}
